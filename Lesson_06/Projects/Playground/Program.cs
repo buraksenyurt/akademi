2ce320beb1fb374775e052d556bf92e54b867c17
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2023-11-28 10:32:53 +0000

    Added a few questions for lesson.

--- Lesson_06/Projects/Playground/Program.cs
+++ Lesson_06/Projects/Playground/Program.cs
@@ -1,9 +1,31 @@
 ﻿namespace Playground;
 
+using System;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Manager workItemManager = new();
+        workItemManager.Seed();
+        var workItems = workItemManager.GetWorkItems();
+        foreach (var wi in workItems)
+        {
+            Console.WriteLine($"{wi.Title}");
+        }
+        //TODO@buraksenyurt Yapılacaklar aşağıdadır
+
+        // Yüksek öncelikli işlerimizi listeyelim
+
+        // Saatlik işlerimizi listeleyelim
+
+        // Saatlik işlerimizin toplam süresini bulalım
+
+        // Önceliği normal olan işlerimizinde gün bazlı olanları listeleyelim
+
+        // Yüksek öncelikli işler için ayırdığımız sürelerin toplamını gün bazında hesap edelim
+
+        // En uzun başlıklı işimizi bulup ekrana yazdıralım
+
     }
 }
