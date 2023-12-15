5d3b79a60a61f49fdcf93bf5d6d214230a5163f3
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2023-12-15 07:53:26 +0000

    Initiated a new homework project.

---
+++ Lesson_08/Projects/Transformer/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Transformer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    static bool CheckArguments(string[] args){
+        return false;
+    }
+}
+
+enum TargetFormat{
+    OnlyBytes,
+    Base64Encoded
+}
