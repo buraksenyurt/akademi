c59a7111ce0569da1a759e241e89dc75f5cfb962
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2023-12-29 09:57:33 +0000

    Added another interface implementation sample.

---
+++ Lesson_10/Projects/Playground3/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Playground3;
+
+internal class Program
+{
+    static void Main()
+    {
+        var johnDoe = new Customer
+        {
+            FirstName = "Jhone",
+            MiddleName = "Junior",
+            LastName = "Doe"
+        };
+        Console.WriteLine(johnDoe.ToString("FL", null));
+        Console.WriteLine(johnDoe.ToString("LF", null));
+        Console.WriteLine(johnDoe.ToString("F", null));
+        Console.WriteLine(johnDoe.ToString("L", null));
+    }
+}
+
+class Customer
+    : IFormattable
+{
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; } = string.Empty;
+    public string LastName { get; set; }
+
+    public string ToString(string? format, IFormatProvider? formatProvider)
+    {
+        _ = formatProvider ?? System.Globalization.CultureInfo.CurrentCulture;
+
+        return format switch
+        {
+            "FL" => $"{FirstName} {MiddleName}, {LastName}",
+            "LF" => $"{LastName},{FirstName} {MiddleName}",
+            "F" => $"{FirstName} {MiddleName}",
+            "L" => $"{LastName}",
+            _ => throw new ArgumentException("This format is not provided"),
+        };
+    }
+}
