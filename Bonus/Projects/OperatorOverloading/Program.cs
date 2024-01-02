6b43c2fe8810bbf1bf139917816459aa5bded83e
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2024-01-02 08:27:36 +0000

    Improved the op overloading.

--- Bonus/Projects/OperatorOverloading/Program.cs
+++ Bonus/Projects/OperatorOverloading/Program.cs
@@ -6,12 +6,16 @@
 
     Bazı operatörler için ters operatörlerin de yüklenmesi gerekir. Örneğin < operatörünü override edersek > operatörünün de override edilmesi gerekir.
 */
+using System.Security.AccessControl;
+
 namespace OperatorOverloading;
 
 class Program
 {
     static void Main()
     {
+        #region Sample #1
+
         var trinity = new Player(1, "Tirinity");
         var neo = new Player(2, "Neo");
         var raven = new Player(3, "Raven");
@@ -46,6 +50,16 @@ class Program
         {
             Console.WriteLine("Trinity ve Raven Eşit güçte oyuncular.");
         }
+        #endregion Sample #1
+
+        #region Sample #2
+
+        var color1 = new Color(125, 0, 0, 0.5);
+        var color2 = new Color(0, 125, 255, 0.75);
+        var newColor = color1 + color2;
+        Console.WriteLine(newColor);
+
+        #endregion Sample #2
     }
 }
 
@@ -134,5 +148,57 @@ class Player
     public override int GetHashCode()
     {
         return ToString().GetHashCode();
+    }
+}
+
+/// <summary>
+/// RGB renk bilgisi tutan nesne
+/// </summary>
+public class Color
+{
+    /// <summary>
+    /// 0..255 arasında kırmızı renk kodu
+    /// </summary>
+    public byte Red { get; }
+    /// <summary>
+    /// 0..255 arasında yeşil renk kodu
+    /// </summary>
+    public byte Green { get; }
+    /// <summary>
+    /// 0..255 arasında mavi renk kodu
+    /// </summary>
+    public byte Blue { get; }
+    /// <summary>
+    /// 0.0 ile 1.0 arasında transparanlık değeri
+    /// </summary>
+    public double Alpha { get; }
+    public Color(byte red, byte green, byte blue, double alpha)
+    {
+        //NOT: Alpha için 0.0 - 1.0 arasında olma hali kontrolü göz ardı edilmiştir.
+        Red = red;
+        Green = green;
+        Blue = blue;
+        Alpha = alpha;
+    }
+    /// <summary>
+    /// Renkleri birleştirerek yeni bir rengin oluşturulmasını sağlar
+    /// </summary>
+    /// <param name="color1">Birinci renk</param>
+    /// <param name="color2">İkinci renk</param>
+    /// <returns></returns>
+    public static Color operator +(Color color1, Color color2)
+    {
+        //NOT: Yeni bir renk elde edilirken 256'dan büyük olan toplamlar için yine 0-256 arası bir değer elde edilemsi işlemi göz ardı edilmiştir
+        return new Color(
+            (byte)(color1.Red + color2.Red)
+            , (byte)(color1.Green + color2.Green)
+            , (byte)(color1.Blue + color2.Blue)
+            , color1.Alpha + color2.Alpha
+        );
+    }
+
+    public override string ToString()
+    {
+        return $"{Red}:{Green}:{Blue} ({Alpha}%)";
     }
 }
