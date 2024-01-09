661b3a99adb9d0bf8709f8d84bb28859e9a9ef7d
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2024-01-09 12:24:56 +0000

    Refactored code.

--- Bonus/Projects/ALittleBitLINQ/Program.cs
+++ Bonus/Projects/ALittleBitLINQ/Program.cs
@@ -13,10 +13,32 @@ ListGames(games_with_ai_expertise);
 var oldest_game = world.GetOldestGame();
 Console.WriteLine($"Oldest game is '{oldest_game.Title}'");
 
+Console.WriteLine("\nGame Titles\n");
+var game_titles = world.GetGameTitles();
+foreach (var title in game_titles)
+{
+    Console.WriteLine(title);
+}
+
+Console.WriteLine("\nGames Ordered by Release Year");
+var ordered_by_release_year = world.OrderByReleaseYear(Order.Descending);
+ListGames(ordered_by_release_year);
+
+Console.WriteLine("\nMulti Skills Programmers");
+var programmers_by_multi_skills = world.FindProgrammersByMultiSkills();
+foreach (var programmer in programmers_by_multi_skills)
+{
+    Console.WriteLine(programmer);
+}
+
 static void ListGames(List<VideoGame> games)
 {
     foreach (var game in games)
     {
-        Console.WriteLine($"{game.Title},{game.ReleaseYear}");
+        Console.WriteLine($"{game.Title},{game.ReleaseYear},{game.Genre}");
+        foreach (var programmer in game.Programmers)
+        {
+            Console.WriteLine($"\t{programmer.Name} ({programmer.Expertise})");
+        }
     }
 }
