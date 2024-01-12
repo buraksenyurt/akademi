78caca032e1254e92651f5e1177ea5bfd43cd148
buraksenyurt <burakselimsenyurt@gmail.com>
2024-01-12 16:52:25 +0300

    Refactored code.

--- Lesson_12/Projects/UseArguments/Program.cs
+++ Lesson_12/Projects/UseArguments/Program.cs
@@ -36,22 +36,44 @@ public interface ICommand
 public class AddCommand
     : ICommand
 {
+    public static int ArgumentCount => 3;
+
     public void Execute(params string[] args)
     {
-        string title = args[1];
-        int size = int.Parse(args[2]);
-        bool isActive = bool.Parse(args[3]);
-        Console.WriteLine($"Girilen komut bilgisi -add. Parametreleri {title} {size} {isActive}");
+        /*
+            Hatalı argüman sayıları veya doğru türe dönüştürülemeyen
+            parametreler için özel exception tipleri de tasarlanabilir.
+        */
+        if (args.Length != ArgumentCount + 1)
+        {
+            Console.WriteLine("Argüman sayısı hatalı. add komutu 3 parametre ile çalışır.");
+        }
+        else
+        {
+            string title = args[1];
+            int size = int.Parse(args[2]);
+            bool isActive = bool.Parse(args[3]);
+            Console.WriteLine($"Girilen komut bilgisi -add. Parametreleri {title} {size} {isActive}");
+        }
     }
 }
 
 public class ListCommand
     : ICommand
 {
+    public static int ArgumentCount => 1;
+
     public void Execute(params string[] args)
     {
-        string query = args[1];
-        Console.WriteLine($"Girilen komut bilgisi -list. Parametresi {query}");
+        if (args.Length != ArgumentCount + 1)
+        {
+            Console.WriteLine("Argüman sayısı hatalı. list komutu 1 parametre ile çalışır.");
+        }
+        else
+        {
+            string query = args[1];
+            Console.WriteLine($"Girilen komut bilgisi -list. Parametresi {query}");
+        }
     }
 }
 
@@ -70,14 +92,11 @@ public class CommandParser
         if (args.Length == 0)
             return new UnkownCommand();
 
-        switch (args[0])
+        return args[0] switch
         {
-            case "-add":
-                return new AddCommand();
-            case "-list":
-                return new ListCommand();
-            default:
-                return new UnkownCommand();
-        }
+            "-add" => new AddCommand(),
+            "-list" => new ListCommand(),
+            _ => new UnkownCommand(),
+        };
     }
 }
